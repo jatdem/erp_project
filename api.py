7de9c58650eafa6fd317be38e5e999b409357e6a
jatdem <yahoooh@naver.com>
2021-02-01 15:31:50 +0900

    주문 100개 이상 나오게 하는 방법 없데이트
    Update how to get more than 100 orders

--- api.py
+++ api.py
@@ -1,12 +1,19 @@
 import requests
+from pprint import pprint
 
-url = "http://playauto-api.playauto.co.kr/emp/v1/orders"
+url = "http://playauto-api.playauto.co.kr/emp/v1/orders/?page=1:2"
+#주문 100건 이상 나오게 하는 방법: 주소 뒤에 /? 추가 하고 파라미터(body sheme 추가)
 
 headers = {
     'user-agent': "vscode-restclient",
     'x-api-key': "0805a02bb9f1318cef8da503a4b7e05c"
     }
 
+# param = 
+
 response = requests.request("GET", url, headers=headers)
 
-print(response.text)
+pprint(response.text)
+
+# with open("apitest.html", "w", encoding="utf-8") as f:
+#     f.write(response.text)
