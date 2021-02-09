b26213d246700f1f6254fbf08ae771185e6948c1
jatdem <yahoooh@naver.com>
2021-02-09 16:18:03 +0900

    pyqt5 파일 일부 수정

--- api.py
+++ api.py
@@ -5,7 +5,7 @@ import pandas as pd
 #주문 100건 이상 나오게 하는 방법: 주소 뒤에 /? 추가 하고 파라미터(body sheme 추가)
 url = "http://playauto-api.playauto.co.kr/emp/v1/orders"
 
-querystring = {"count":"10"}
+querystring = {"count":"20"}
 headers = {
     'user-agent': "vscode-restclient",
     'x-api-key': "0805a02bb9f1318cef8da503a4b7e05c"
