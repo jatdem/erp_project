cb1f40dab2e246d3b73c2547d2f29631a882e034
jatdem <yahoooh@naver.com>
2021-03-08 17:41:30 +0900

    위젯추가

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import tkinter.ttk
 from pandastable import Table
 import pandas as pd
 import requests
+from tkcalendar import DateEntry
 
 
 #전체주문 api
@@ -57,38 +58,71 @@ frame2 = Frame(root, relief="solid", bd=1)
 frame2.pack(fill="both", expand=True)
 ntb.add(frame2, text="주문관리")
 
-ent_btn = Button(frame2, text="전체보기")
-ent_btn.grid(row=0, column=0)
-new_btn = Button(frame2, text="신규주문")
-new_btn.grid(row=0, column=1)
-cf_btn = Button(frame2, text="주문확인")
-cf_btn.grid(row=1, column=1)
-pt_btn = Button(frame2, text="보류")
-pt_btn.grid(row=2, column=1)
-op_btn = Button(frame2, text="송장출력")
-op_btn.grid(row=0, column=2)
-ip_btn = Button(frame2, text="송장입력")
-ip_btn.grid(row=1, column=2)
-rl_btn = Button(frame2, text="출고")
-rl_btn.grid(row=2, column=2)
-dl_btn = Button(frame2, text="배송중")
-dl_btn.grid(row=0, column=3)
-rc_btn = Button(frame2, text="수취확인")
-rc_btn.grid(row=1, column=3)
-cn_btn = Button(frame2, text="취소")
-cn_btn.grid(row=2, column=3)
-re_btn = Button(frame2, text="반품요청")
-re_btn.grid(row=0, column=4)
-ex_btn = Button(frame2, text="교환요청")
-ex_btn.grid(row=1, column=4)
+ent_btn = Button(frame2, text="전체보기", height=4) #버튼배치 시작
+new_btn = Button(frame2, text="신규주문", width=6)
+cf_btn = Button(frame2, text="주문확인", width=6,height=1)
+pt_btn = Button(frame2, text="보류", width=6,height=1)
+op_btn = Button(frame2, text="송장출력", width=6,height=1)
+ip_btn = Button(frame2, text="송장입력", width=6,height=1)
+rl_btn = Button(frame2, text="출고", width=6,height=1)
+dl_btn = Button(frame2, text="배송중", width=6,height=1)
+rc_btn = Button(frame2, text="수취확인", width=6,height=1)
+cn_btn = Button(frame2, text="취소", width=6,height=1)
+re_btn = Button(frame2, text="반품요청", width=6,height=1)
+ex_btn = Button(frame2, text="교환요청", width=6,height=1)
+ent_btn.place(x=2, y=4) #버튼좌표 설정 시작
+new_btn.place(x=63, y=0)
+cf_btn.place(x=63, y=27)
+pt_btn.place(x=63, y=54)
+op_btn.place(x=118, y=0)
+ip_btn.place(x=118, y=27)
+rl_btn.place(x=118, y=54)
+dl_btn.place(x=173, y=0)
+rc_btn.place(x=173, y=27)
+cn_btn.place(x=173, y=54)
+re_btn.place(x=228, y=0)
+ex_btn.place(x=228, y=27)
+
+src_lb = Label(frame2, text="검색") #검색 레이블
+date_lb = Label(frame2, text="날짜") #날짜 레이블
+src_ent = Entry(frame2) #검색 창
+date_ent1 = Entry(frame2, width=10) #날짜 창1
+date_ent2 = Entry(frame2, width=10) #날짜 창2
+src_lb.place(x=290, y=0)
+src_ent.place(x=330, y=0)
+date_lb.place(x=290, y=21)
+date_ent1.place(x=330, y=21)
+date_ent2.place(x=408, y=21)
+
+src_btn = Button(frame2, text="검색시작", height=4) #텍스트 검색 버튼
+src_btn.place(x=484)
+
+st_list = Listbox(frame2) #사이트 프레임(라벨)
+st_list.place(x=2, y=100, width=160, height=600)
+st_list.insert(1, "씨제이")
+st_list.insert(2, "롯데닷컴")
+st_list.insert(3, "에이케이몰")
+st_list.insert(4, "신세계통합")
+st_list.insert(5, "네이버")
+st_list.insert(6, "롯데아이몰")
+st_list.insert(7, "11번가")
+st_list.insert(8, "현대몰")
+st_list.insert(9, "롯데온")
+st_list.insert(10, "쿠팡")
+st_list.insert(11, "자사몰")
+st_list.insert(12, "위메프")
+st_list.insert(12, "직접입력")
+
+od_frame = LabelFrame(frame2, text = "전체주문") #주문 프레임(라벨)
+od_frame.place(x=170, y=100, width=1738)
+ent_order_pt = Table(od_frame, dataframe=df1) #주문 데이터프레임 출력
+ent_order_pt.show()
 
 #frame3의 내용
 frame3 = Frame(root, relief="solid", bd=1)
 frame3.pack(fill="both", expand=True)
 ntb.add(frame3, text="재고관리")
+ 
 
-#데이터테이블
-# ent_order_pt = Table(frame2, dataframe=df1)
-# ent_order_pt.show()
 
 root.mainloop()
