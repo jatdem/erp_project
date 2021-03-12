511256976370b8acaacda1927da7a8dc9bc5e7bd
jatdem <yahoooh@naver.com>
2021-03-12 17:25:44 +0900

    tkinter update

--- main.py
+++ main.py
@@ -1,50 +1,51 @@
-#-*- coding:utf-8 -*-
+# -*- coding:utf-8 -*-
 from tkinter import *
-import tkinter.ttk
+import tkinter.ttk as ttk
 from pandastable import Table
 import pandas as pd
 import requests
 from tkcalendar import DateEntry
 
 
-#전체주문 api
+# 전체주문 api
 url = "http://playauto-api.playauto.co.kr/emp/v1/orders"
 
-querystring = {"startDate":"20210220","endDate":"20210305","count":"10"}
+querystring = {"startDate": "20210220", "endDate": "20210305", "count": "10"}
 headers = {
     'user-agent': "vscode-restclient",
     'x-api-key': "0805a02bb9f1318cef8da503a4b7e05c"
-    }
- 
+}
+
 response = requests.request("GET", url, headers=headers, params=querystring)
 
 res_json = response.json()
 
 df = pd.json_normalize(res_json)
-df1 = df.drop({"UniqueId","SiteCode"},axis = "columns") #,"OptCode"
-df1.columns = ["DB번호","사이트","ID","사용자","수집일","주문일","결제일","송장전송일","상태변경일","배송예정일","상태","주문번호","상품코드","상품명","옵션명","옵션가",\
-               "추가옵션","추가옵션가","원가","공급가","판매가","수량","배송방법","배송비","주문자아이디","주문자","주문자전화번호","주문자핸드폰","주문자이메일","수령자","수령자영문이름",\
-               "수령자전화번호","수령자핸드폰","수령자우편번호","수령자주소","배송메세지","배송사","송장번호","마스터상품코드","판매자상품코드","기타메세지","주문자아이디","사은품",\
-               "수령자주민등록번호(통관용)","그룹키"]
+df1 = df.drop({"UniqueId", "SiteCode"}, axis="columns")  # ,"OptCode"
+df1.columns = ["DB번호", "사이트", "ID", "사용자", "수집일", "주문일", "결제일", "송장전송일", "상태변경일", "배송예정일", "상태", "주문번호", "상품코드", "상품명", "옵션명", "옵션가",
+               "추가옵션", "추가옵션가", "원가", "공급가", "판매가", "수량", "배송방법", "배송비", "주문자아이디", "주문자", "주문자전화번호", "주문자핸드폰", "주문자이메일", "수령자", "수령자영문이름",
+               "수령자전화번호", "수령자핸드폰", "수령자우편번호", "수령자주소", "배송메세지", "배송사", "송장번호", "마스터상품코드", "판매자상품코드", "기타메세지", "주문자아이디", "사은품",
+               "수령자주민등록번호(통관용)", "그룹키"]
 
-#메인
-root=Tk()
+# 메인
+root = Tk()
 root.title("Formongde")
-root.geometry("1000x800")
+root.geometry("1800x1000+50+5")
 root.resizable(True, True)
 
-#노트북
-ntb = tkinter.ttk.Notebook(root,height=1800)
+# 노트북
+ntb = ttk.Notebook(root, height=1800)
 ntb.pack(fill="both")
 
 # ntb = ttk.Notebook()
 
-#프레임1의 내용
+# 홈 프레임1의 내용
 frame1 = Frame(root, relief="solid", bd=1)
 frame1.pack(fill="both", expand=True)
-ntb.add(frame1,text='홈')
+ntb.add(frame1, text='홈')
 
-od_box = Listbox(frame1,bg="#FFFFF0",selectbackground="#FFFFF0") #,bg="#BC8F8F"
+od_box = Listbox(frame1, bg="#FFFFF0",
+                 selectbackground="#FFFFF0")  # ,bg="#BC8F8F"
 od_box.place(x=10, y=10)
 td_od_lb = Label(od_box, text="오늘주문")
 td_od_lb.grid(row=1, column=1)
@@ -53,24 +54,24 @@ td_od_lb2.grid(row=2, column=1)
 td_od_lb3 = Label(od_box, text="33,402,600 원")
 td_od_lb3.grid(row=3, column=1)
 
-#frame2의 내용
+# 주문관리 frame2의 내용
 frame2 = Frame(root, relief="solid", bd=1)
 frame2.pack(fill="both", expand=True)
 ntb.add(frame2, text="주문관리")
 
-ent_btn = Button(frame2, text="전체보기", height=4) #버튼배치 시작
+ent_btn = Button(frame2, text="전체보기", height=4)  # 버튼배치 시작
 new_btn = Button(frame2, text="신규주문", width=6)
-cf_btn = Button(frame2, text="주문확인", width=6,height=1)
-pt_btn = Button(frame2, text="보류", width=6,height=1)
-op_btn = Button(frame2, text="송장출력", width=6,height=1)
-ip_btn = Button(frame2, text="송장입력", width=6,height=1)
-rl_btn = Button(frame2, text="출고", width=6,height=1)
-dl_btn = Button(frame2, text="배송중", width=6,height=1)
-rc_btn = Button(frame2, text="수취확인", width=6,height=1)
-cn_btn = Button(frame2, text="취소", width=6,height=1)
-re_btn = Button(frame2, text="반품요청", width=6,height=1)
-ex_btn = Button(frame2, text="교환요청", width=6,height=1)
-ent_btn.place(x=2, y=4) #버튼좌표 설정 시작
+cf_btn = Button(frame2, text="주문확인", width=6, height=1)
+pt_btn = Button(frame2, text="보류", width=6, height=1)
+op_btn = Button(frame2, text="송장출력", width=6, height=1)
+ip_btn = Button(frame2, text="송장입력", width=6, height=1)
+rl_btn = Button(frame2, text="출고", width=6, height=1)
+dl_btn = Button(frame2, text="배송중", width=6, height=1)
+rc_btn = Button(frame2, text="수취확인", width=6, height=1)
+cn_btn = Button(frame2, text="취소", width=6, height=1)
+re_btn = Button(frame2, text="반품요청", width=6, height=1)
+ex_btn = Button(frame2, text="교환요청", width=6, height=1)
+ent_btn.place(x=2, y=4)  # 버튼좌표 설정 시작
 new_btn.place(x=63, y=0)
 cf_btn.place(x=63, y=27)
 pt_btn.place(x=63, y=54)
@@ -83,21 +84,21 @@ cn_btn.place(x=173, y=54)
 re_btn.place(x=228, y=0)
 ex_btn.place(x=228, y=27)
 
-src_lb = Label(frame2, text="검색") #검색 레이블
-date_lb = Label(frame2, text="날짜") #날짜 레이블
-src_ent = Entry(frame2) #검색 창
-date_ent1 = Entry(frame2, width=10) #날짜 창1
-date_ent2 = Entry(frame2, width=10) #날짜 창2
+src_lb = Label(frame2, text="검색")  # 검색 레이블
+date_lb = Label(frame2, text="날짜")  # 날짜 레이블
+src_ent = Entry(frame2)  # 검색 창
+date_ent1 = Entry(frame2, width=10)  # 날짜 창1
+date_ent2 = Entry(frame2, width=10)  # 날짜 창2
 src_lb.place(x=290, y=0)
 src_ent.place(x=330, y=0)
 date_lb.place(x=290, y=21)
 date_ent1.place(x=330, y=21)
 date_ent2.place(x=408, y=21)
 
-src_btn = Button(frame2, text="검색시작", height=4) #텍스트 검색 버튼
+src_btn = Button(frame2, text="검색시작", height=4)  # 텍스트 검색 버튼
 src_btn.place(x=484)
 
-st_list = Listbox(frame2) #사이트 프레임(라벨)
+st_list = Listbox(frame2)  # 사이트 프레임(라벨)
 st_list.place(x=2, y=100, width=160, height=600)
 st_list.insert(1, "씨제이")
 st_list.insert(2, "롯데닷컴")
@@ -113,16 +114,48 @@ st_list.insert(11, "자사몰")
 st_list.insert(12, "위메프")
 st_list.insert(12, "직접입력")
 
-od_frame = LabelFrame(frame2, text = "전체주문") #주문 프레임(라벨)
+od_frame = LabelFrame(frame2, text="전체주문")  # 주문 프레임(라벨)
 od_frame.place(x=170, y=100, width=1738)
-ent_order_pt = Table(od_frame, dataframe=df1) #주문 데이터프레임 출력
+ent_order_pt = Table(od_frame, dataframe=df1)  # 주문 데이터프레임 출력
 ent_order_pt.show()
 
-#frame3의 내용
-frame3 = Frame(root, relief="solid", bd=1)
+# 재고관리 frame3의 내용
+frame3 = Frame(root, relief="solid", bd=1)  # 프레임3 생성
 frame3.pack(fill="both", expand=True)
 ntb.add(frame3, text="재고관리")
- 
+
+stock_frame = LabelFrame(frame3)  # 라벨프레임 생성
+stock_frame.place(x=155, width=1800, height=1000)
+
+menu_tv = ttk.Treeview(frame3)  # 트리뷰 생성
+menu_tv.place(width=150)
+
+
+def stock_table(event):  # 재고표 라벨프레임에 출력해주는 함수
+    pass
+
+
+menu_tv.insert('', '0', 'item1', text="완제품 재고")
+menu_tv.insert('', '1', 'item2', text="원단재고")
+menu_tv.insert('', '2', 'item3', text="일자별 판매량")
+menu_tv.insert('', '3', 'item4', text="외주")
+menu_tv.insert('', '4', 'item5', text="비고1")
+menu_tv.insert('', '5', 'item6', text="비고2")
+
+# 택배관리 프레임4의 내용
+frame4 = Frame(root, relief="solid", bd=1)
+frame4.pack(fill="both", expand=True)
+ntb.add(frame4, text="택배관리")
+
+dilivery_tv = ttk.Treeview(frame4)  # 택배관리 트리뷰 생성
+dilivery_tv.place(width=150)
+dilivery_tv.insert('', '0', 'item1', text="출고")
+dilivery_tv.insert('', '1', 'item2', text="송장")
+dilivery_tv.insert('', '2', 'item3', text="비고1")
+dilivery_tv.insert('', '3', 'item4', text="비고2")
+dilivery_tv.insert('', '4', 'item5', text="비고3")
+dilivery_tv.insert('', '5', 'item6', text="비고4")
+# menu_tv.bind("<ButtonRelese-1>", stock_table) # 트리뷰 클릭시 재고출력 바인딩
 
 
 root.mainloop()
